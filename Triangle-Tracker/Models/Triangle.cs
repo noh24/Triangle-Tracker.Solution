8080f2bb0468f57ef83b173162cd763695686002
Brian Noh <noh24@ymail.com>
2022-12-06 17:29:44 -0500

    add code to clear triangle contrustor test
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- Triangle-Tracker/Models/Triangle.cs
+++ Triangle-Tracker/Models/Triangle.cs
@@ -2,20 +2,29 @@ namespace TriangleTracker
 {
   public class Triangle
   {
-    public bool IsLeapYear(int year)
+    // auto properties
+    public int Side1 {get; set;}
+    public int Side2 {get; set;}
+    public int Side3 {get; set;}
+    // constructor
+    public Triangle(int side1, int side2, int side3)
     {
-      if (year % 400 == 0)
-      {
-        return true;
-      }
-      else if (year % 100 == 0)
-      {
-        return false;
-      }
-      else
-      {
-        return year % 4 == 0;
-      }
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
     }
+    // method
+      // blah
   }
 }
+//constructor 3arg 
+  // side 1
+  // side 2
+  // side 3
+
+// TypeOfTriangle() -> Method
+  // side1 + side2 < side3
+  // If any side is longer than the other two sides combined, the lengths cannot form a triangle. 
+    // Equilateral: All sides are equal; side = side 
+    // Isosceles: Exactly 2 sides are equal; 
+    // Scalene: No sides are equal.
