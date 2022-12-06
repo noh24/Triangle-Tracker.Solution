f37414a21621a1aea10b4170f8561d814f45bb8f
Brian Noh <noh24@ymail.com>
2022-12-06 17:42:17 -0500

    add code to pass type of triangle test
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- Triangle-Tracker/Models/Triangle.cs
+++ Triangle-Tracker/Models/Triangle.cs
@@ -14,17 +14,29 @@ namespace TriangleTracker
       Side3 = side3;
     }
     // method
-      // blah
+    public string TypeOfTriangle()
+    {
+      // If any side is longer than the other two sides combined, the lengths cannot form a triangle.
+      if (Side1 > (Side2 + Side3) || Side2 > (Side1 + Side3) || Side3 > (Side1 + Side2))
+      {
+        return "not triangle";
+      } else {
+        // Equilateral: All sides are equal; side = side 
+        if (Side1 == Side2 && Side2 == Side3)
+        {
+          return "equilateral";
+        } 
+        // Isosceles: Exactly 2 sides are equal
+        else if (Side1 == Side2 || Side2 == Side3 || Side1 == Side3)
+        {
+          return "isosceles";
+        } 
+        // Scalene: No sides are equal
+        else
+        {
+          return "scalene";
+        }
+      }
+    }
   }
 }
-//constructor 3arg 
-  // side 1
-  // side 2
-  // side 3
-
-// TypeOfTriangle() -> Method
-  // side1 + side2 < side3
-  // If any side is longer than the other two sides combined, the lengths cannot form a triangle. 
-    // Equilateral: All sides are equal; side = side 
-    // Isosceles: Exactly 2 sides are equal; 
-    // Scalene: No sides are equal.
